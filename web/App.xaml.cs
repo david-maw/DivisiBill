e4f657b6b2e81cdb3a5a276cce42789230c46fa1
David Maw <david@maw.name>
2024-11-15 20:03:58 -0800

    Update web Project to .NET 9

--- web/App.xaml.cs
+++ web/App.xaml.cs
@@ -5,14 +5,12 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-
-        MainPage = new AppShell();
     }
     protected override Window CreateWindow(IActivationState? activationState)
     {
         ArgumentNullException.ThrowIfNull(activationState);
 
-        Window window = base.CreateWindow(activationState);
+        Window window = new(new AppShell());
 
         // Set the App window to a sensible (phone like) size
         if (DeviceInfo.Idiom == DeviceIdiom.Desktop || DeviceInfo.Idiom == DeviceIdiom.Tablet)
