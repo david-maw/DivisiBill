0b0cf9940bdd28489f09773652d392213a53f41b
David Maw <david@maw.name>
2025-02-08 11:17:14 -0800

    Add IsNegativeConverter and update label styles
    Move the handling of displaying negative numbers to XAML.
    
    Introduced a new `IsNegativeConverter` to determine if values are negative, supporting various numeric types and strings. Updated `App.xaml` to include this converter and a new `NegativeNumber` color resource. In `LineItemsPage.xaml`, created a `SignedLabelStyle` for labels that changes text color based on negativity, enhancing visual distinction for negative amounts.

---
+++ DivisiBill/Services/IsNegativeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace DivisiBill.Services;
+
+internal class IsNegativeConverter : IValueConverter
+{
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo cultureInfo) => value is null
+            ? false
+            : value switch
+            {
+                string s => float.TryParse(s, out float x) && x < 0,
+                sbyte x => x < 0,
+                byte x => x < 0,
+                short x => x < 0,
+                ushort x => x < 0,
+                int x => x < 0,
+                uint x => x < 0,
+                long x => x < 0,
+                ulong x => x < 0,
+                float x => x < 0,
+                double x => x < 0,
+                decimal x => x < 0,
+                _ => (object)false,
+            };
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo cultureInfo) => throw new NotImplementedException();
+}
