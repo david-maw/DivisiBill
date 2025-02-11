20caf3804c45fdaf89decf92a631bd6ca93a716e
David Maw <david@maw.name>
2025-02-11 10:35:25 -0800

    Eliminate Warnings form TrueToStringConverter

--- DivisiBill/Services/TrueToStringConverter.cs
+++ DivisiBill/Services/TrueToStringConverter.cs
@@ -5,8 +5,12 @@ namespace DivisiBill.Services;
 internal class TrueToStringConverter : IValueConverter
 {
     // Returns either the parameter or an empty string
-    public object Convert(object value, Type targetType, object parameter, CultureInfo cultureInfo) => value is bool && parameter is not null && parameter is string
-            ? (object)((bool)value ? "" : (string)parameter)
-            : throw new ArgumentException();
+    public object Convert(object value, Type targetType, object parameter, CultureInfo cultureInfo) =>
+        value is not bool boolValue
+            ? throw new ArgumentException("Not a boolean value", nameof(value))
+            : parameter is not string and not null
+            ? throw new ArgumentException("Not a string", nameof(parameter))
+            : (object)(boolValue ? "" : (string)parameter);
+
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo cultureInfo) => throw new NotImplementedException();
 }
