14721dd3e8d74a33d2bcb04011769960d191a7a4
David Maw <david@maw.name>
2025-02-14 11:26:22 -0800

    Improve Format of "Telemetry" Popup
    During initialization DivisiBill may ask the user whether they wish to send telemetry in the event of a failure. The appearance of this dialog has been improved.
    
    Updated QuestionPage.xaml to implement a toolkit:Popup with a transparent background, rounded border, and improved layout. Adjusted padding, background color, and spacing for better visual consistency. CanBeDismissedByTappingOutsideOfPopup is set to False to guarantee to call Button_Clicked in order to set up a result object.

--- DivisiBill/Views/QuestionPage.xaml.cs
+++ DivisiBill/Views/QuestionPage.xaml.cs
@@ -1,5 +1,6 @@
-﻿namespace DivisiBill.Views;
+﻿using System.Runtime.CompilerServices;
 
+namespace DivisiBill.Views;
 public partial class QuestionPage : CommunityToolkit.Maui.Views.Popup
 {
     public QuestionPage(string titleParam, string textParam, bool initialYes)
@@ -18,53 +19,18 @@ public partial class QuestionPage : CommunityToolkit.Maui.Views.Popup
         // Close the dialog
         Close(d);
     }
-
-    public bool Yes
+    public bool Yes { get; set => SetProperty(ref field, value); } = false;
+    public bool AskAgain { get; set => SetProperty(ref field, value); } = true; // Must start out true or we wouldn't be in this page
+    public string Title { get; set => SetProperty(ref field, value); }
+    public string Text { get; set => SetProperty(ref field, value); }
+    protected bool SetProperty<T>(ref T backingStore, T value, Action onChanged = null,
+    [CallerMemberName] string propertyName = "")
     {
-        get;
-        set
-        {
-            if (field != value)
-            {
-                field = value;
-                OnPropertyChanged();
-            }
-        }
-    } = false;
-    public bool AskAgain
-    {
-        get;
-        set
-        {
-            if (field != value)
-            {
-                field = value;
-                OnPropertyChanged();
-            }
-        }
-    } = true; // Must start out true or we wouldn't be in this page
-    public string Title
-    {
-        get;
-        set
-        {
-            if (field != value)
-            {
-                field = value;
-                OnPropertyChanged();
-            }
-        }
-    }
-    public string Text
-    {
-        get;
-        set
-        {
-            if (field != value)
-            {
-                field = value;
-                OnPropertyChanged();
-            }
-        }
+        if (EqualityComparer<T>.Default.Equals(backingStore, value))
+            return false;
+        backingStore = value;
+        onChanged?.Invoke();
+        OnPropertyChanged(propertyName);
+        return true;
     }
 }
