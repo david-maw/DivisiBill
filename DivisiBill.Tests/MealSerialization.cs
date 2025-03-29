cbccb24f08e527c7e9c71dc8ec2bfae6b499f318
David Maw <david@maw.name>
2025-03-28 20:54:35 -0700

    Add Meal Deserialization Tests
    - Updated `DivisiBill.Tests.csproj` to use preview language features so as to be able to use 'field' in properties.
    - Added necessary using directives and `TestClass` attribute in `MealSerialization.cs`.
    - Introduced `DefaultMeal` property for lazy initialization of `Meal` object.
    - Implemented `DeserializeXml` test method to validate meal deserialization.

--- DivisiBill.Tests/MealSerialization.cs
+++ DivisiBill.Tests/MealSerialization.cs
@@ -1,4 +1,10 @@
-﻿namespace DivisiBill.Tests;
+﻿using DivisiBill.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace DivisiBill.Tests;
+
+[TestClass]
 public class MealSerialization
 {
     internal const string DefaultMealXml = """
@@ -61,4 +67,58 @@ public class MealSerialization
         </Meal>
         """; // End of FakeBillXml
 
+    private Meal DefaultMeal
+    {
+        get
+        {
+            if (field == null)
+            {
+                byte[] byteArray = Encoding.UTF8.GetBytes(MealSerialization.DefaultMealXml);
+                using MemoryStream memoryStream = new(byteArray);
+                field = Meal.LoadFromStream(memoryStream);
+            }
+            return field;
+        }
+    }
+
+    /// <summary>
+    /// Create a Meal from XML
+    /// </summary>
+    [TestMethod]
+    public void DeserializeXml()
+    {
+        Meal meal = DefaultMeal;
+        Assert.IsNotNull(meal);
+        Assert.AreEqual("Queasy Diner", meal.VenueName);
+        Assert.AreEqual("1.1", meal.DataVersion);
+        Assert.AreEqual(DateTime.MinValue, meal.CreationTime);
+        Assert.AreEqual("00010101000000.xml", meal.FileName);
+        Assert.IsTrue(meal.IsDefault);
+        // Verify bill totals
+        Assert.AreEqual(125, meal.SubTotal);
+        Assert.AreEqual(170, meal.RoundedAmount);
+        Assert.AreEqual(9.69m, meal.Tax);
+        Assert.AreEqual(35, meal.Tip);
+        Assert.AreEqual(5, meal.UnallocatedAmount);
+        Assert.AreEqual(169.69m, meal.TotalAmount);
+        Assert.AreEqual(0.0775, meal.TaxRate);
+        Assert.AreEqual(0.20, meal.TipRate);
+        // Verify individual item amounts 
+        Assert.AreEqual(7, meal.LineItems.Count);
+        Assert.AreEqual(20, meal.LineItems[0].Amount);
+        Assert.AreEqual(-10, meal.LineItems[1].Amount);
+        Assert.AreEqual(30, meal.LineItems[2].Amount);
+        Assert.AreEqual(40, meal.LineItems[3].Amount);
+        Assert.AreEqual(60, meal.LineItems[4].Amount);
+        Assert.AreEqual(20, meal.LineItems[5].Amount);
+        Assert.AreEqual(5, meal.LineItems[6].Amount);
+        // Verify costs
+        Assert.AreEqual(3, meal.Costs.Count);
+        Assert.AreEqual("Bill", meal.Costs[0].Nickname);
+        Assert.AreEqual("Chris", meal.Costs[1].Nickname);
+        Assert.AreEqual("Craig", meal.Costs[2].Nickname);
+        Assert.AreEqual(64.03m, meal.Costs[0].Amount);
+        Assert.AreEqual(56.03m, meal.Costs[1].Amount);
+        Assert.AreEqual(43.25m, meal.Costs[2].Amount);
+    }
 }
