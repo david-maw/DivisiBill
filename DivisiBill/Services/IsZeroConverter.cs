ca12e83ef15f132deb1795602eaac21e5640aa29
David Maw <david@maw.name>
2026-01-03 11:14:59 -0800

    Add a Couple More handy Converters for XAML

---
+++ DivisiBill/Services/IsZeroConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace DivisiBill.Services;
+
+public class IsZeroConverter : IValueConverter
+{
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo cultureInfo) => value is null
+            ? false
+            : value switch
+            {
+                sbyte x => x == 0,
+                byte x => x == 0,
+                short x => x == 0,
+                ushort x => x == 0,
+                int x => x == 0,
+                uint x => x == 0,
+                long x => x == 0,
+                ulong x => x == 0,
+                float x => x == 0,
+                double x => x == 0,
+                decimal x => x == 0,
+                _ => (object)false,
+            };
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo cultureInfo) => throw new NotImplementedException();
+}
