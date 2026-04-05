e1246d9dac5426a8f7cfadb2076799d3b857ba58
David Maw <david@maw.name>
2026-04-05 15:53:10 -0700

    Comment and clean up Script.js
    Remove unused function "index()"

--- web/script.js
+++ web/script.js
@@ -1,18 +1,21 @@
-﻿let li = window.location.href.lastIndexOf("#");
+﻿// Extract the anchor (fragment identifier) from the current URL
+let li = window.location.href.lastIndexOf("#");
 var s;
 if (li >= 0) {
     s = window.location.href.substring(li + 1);
 }
 
+// If an anchor was found, highlight the corresponding element
 if (s != null && s.length > 0) {
     let e = document.getElementById(s);
     if (e != null) {
         let h = e.innerHTML;
+        // Wrap the element's content in a <mark> tag to highlight it
         e.innerHTML = "<mark>" + h + "</mark>";
     }
 }
-function index() { gotopage("index.html"); }
+
+// Navigate to the specified page
 function gotopage(page) {
-    //alert("gotopage("+page+") called, about to switch pages")
     window.location.href = page
 }
