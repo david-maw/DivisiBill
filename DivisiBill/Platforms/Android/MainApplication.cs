822d87cabfbd55b2dacc4f2d32937d96de279578
David Maw <david@maw.name>
2026-06-26 17:40:29 -0700

    Allow Debug Direct Access to Web Service
    The DivisiBill project is now sensitive to the values of environment variables DIVISIBILL_WS_(URI/KEY)_DEBUG(A/W) to set direct paths to the debug web service running on a development system. This can be especially useful when the Internet is not available so ngrok cannot be used. The  Android debug build also sets "UsesCleartextTraffic" to permit HTTP access out of the debug Android build.

--- DivisiBill/Platforms/Android/MainApplication.cs
+++ DivisiBill/Platforms/Android/MainApplication.cs
@@ -3,13 +3,17 @@ using Android.Runtime;
 
 namespace DivisiBill.Platforms.Android;
 
-[Application]
-public class MainApplication : MauiApplication
-{
-    public MainApplication(nint handle, JniHandleOwnership ownership)
-        : base(handle, ownership)
-    {
-    }
 
+// This is the main application class for the Android platform in a .NET MAUI application.
+// Allow cleartext traffic (http://) for debug builds so as to reach the debug web service.
+[Application(UsesCleartextTraffic =
+#if DEBUG
+    true
+#else
+    false
+#endif
+)]
+public class MainApplication(nint handle, JniHandleOwnership ownership) : MauiApplication(handle, ownership)
+{
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
